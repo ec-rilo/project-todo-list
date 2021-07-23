a756311749c53a7410af5edd264943396e2cbd94
Edgar Carrillo <ecarrillo046@gmail.com>
2021-07-22 17:59:56 -0700

    Renamed index.html in src to index.ejs

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Project-Todo List',
-      template: './src/index.html'
+      template: './src/index.ejs'
     })
   ],
   module: {
