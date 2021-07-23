a0c285df6aa16ba5b58e4a68a9da1212d3cfb2b8
Edgar Carrillo <ecarrillo046@gmail.com>
2021-07-23 07:45:41 -0700

    Remove hamburger button that is a png

--- src/modules/landing-pg.js
+++ src/modules/landing-pg.js
@@ -4,17 +4,14 @@ function createNavBar() {
     const navBarContainer = document.createElement('div');
     navBarContainer.classList.add('nav-bar-container');
 
-    const hamburgerMenuBtn = document.createElement('img');
-    hamburgerMenuBtn.classList.add('small-btn', 'menu-btn');
-    hamburgerMenuBtn.setAttribute('src', hamburgerMenuImg);
-    navBarContainer.appendChild(hamburgerMenuBtn);
+    
 
     return navBarContainer;
 }
 
 let createLandingPage = (() => {
     const content = document.querySelector('#content');
-    
+
     const navBar = createNavBar();
     
     content.appendChild(navBar);
