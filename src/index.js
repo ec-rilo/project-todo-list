4ddaf2201a95a3b765c4860d86a5deeac78eef8c
Edgar Carrillo <ecarrillo046@gmail.com>
2021-07-26 12:35:53 -0700

    Add local storage for inbox, today, and this week notes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,27 @@ import './styles/main.css';
 import {tabPagePopulation} from './modules/tab-logic.js';
 import {defaultTabLoad} from './modules/tab-logic.js';
 
+let inboxNotesArr = [];
+localStorage.setItem('inboxNotesArr', JSON.stringify(inboxNotesArr));
+
+let todayNotesArr = [];
+localStorage.setItem('todayNotesArr', JSON.stringify(todayNotesArr));
+
+let thisWeekNotesArr = [];
+localStorage.setItem('thisWeekNotesArr', JSON.stringify(thisWeekNotesArr));
+
+// let storedInboxNotes = JSON.parse(localStorage.getItem('inboxNotes'));
+// The above is how to call the array set in local storage
+
 init();
 
 function init() {
     defaultTabLoad();
     tabPagePopulation();
+}
+
+export {
+    inboxNotesArr,
+    todayNotesArr,
+    thisWeekNotesArr
 }
