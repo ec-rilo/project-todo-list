cba17760cdb27e054eb145d5f95b6fca2a1720f2
Edgar Carrillo <ecarrillo046@gmail.com>
2021-07-29 13:48:54 -0700

    Add a function that creates a card asking for to do information

--- src/modules/note-logic.js
+++ src/modules/note-logic.js
@@ -1,3 +1,4 @@
+import { sub } from 'date-fns';
 import {inboxNotesArr} from '../index.js';
 import {todayNotesArr} from '../index.js';
 import {thisWeekNotesArr} from '../index.js';
@@ -53,6 +54,153 @@ class Tab {
 }
 
 /* DOM creation functions */
+
+function createCard() {
+    let card = document.createElement('form');
+    card.classList.add('card');
+    card.setAttribute('autocomplete', 'off');
+    card.setAttribute('action', '#');
+    card.setAttribute('method', 'GET');
+
+    let cardContentContainer = document.createElement('div')
+    cardContentContainer.classList.add('card-content-container');
+    card.appendChild(cardContentContainer);
+
+    let cardTitleContainer = document.createElement('div');
+    cardTitleContainer.classList.add('card-title-container');
+    cardContentContainer.appendChild(cardTitleContainer);
+
+    let title = document.createElement('input');
+    title.setAttribute('id', 'title-input');
+    title.classList.add('card-title-input');
+    title.setAttribute('type', 'text');
+    title.setAttribute('maxLength', '20');
+    title.setAttribute('placeholder', 'Enter Task Name');
+    title.setAttribute('required', '');
+    cardTitleContainer.appendChild(title);
+
+    let closeBtn = document.createElement('a');
+    closeBtn.setAttribute('href', '#');
+    closeBtn.classList.add('close');
+    closeBtn.setAttribute('tabindex', '0');
+    closeBtn.setAttribute('role', 'button');
+    closeBtn.innerHTML = 'close';
+    cardTitleContainer.appendChild(closeBtn);
+
+    let horizontalLine = document.createElement('div');
+    horizontalLine.classList.add('horizontal-line');
+    cardContentContainer.appendChild(horizontalLine);
+
+    let cardContent = document.createElement('div');
+    cardContent.classList.add('card-content');
+    cardContentContainer.appendChild(cardContent);
+
+    let descriptionContainer = document.createElement('div');
+    descriptionContainer.classList.add('card-description-container');
+    cardContent.appendChild(descriptionContainer);
+
+    let descriptionLabel = document.createElement('label');
+    descriptionLabel.classList.add('input-title');
+    descriptionLabel.setAttribute('for', 'description-input');
+    descriptionLabel.innerHTML = 'Description:';
+    descriptionContainer.appendChild(descriptionLabel);
+
+    let descriptionInput = document.createElement('textarea');
+    descriptionInput.classList.add('input-styling');
+    descriptionInput.setAttribute('id', 'description-input');
+    descriptionInput.setAttribute('maxLength', '400');
+    descriptionInput.setAttribute('required', '');
+    descriptionContainer.appendChild(descriptionInput);
+
+    let dateContainer = document.createElement('div');
+    dateContainer.classList.add('card-date-container');
+    cardContent.appendChild(dateContainer);
+
+    let dateLabel = document.createElement('label');
+    dateLabel.classList.add('input-title');
+    dateLabel.setAttribute('for', 'date-input');
+    dateLabel.innerHTML = 'Due Date:';
+    dateContainer.appendChild(dateLabel);
+
+    let dateInput = document.createElement('input');
+    dateInput.classList.add('input-styling', 'card-input');
+    dateInput.setAttribute('id', 'date-input');
+    dateInput.setAttribute('type', 'date');
+    dateInput.setAttribute('required', '');
+    dateContainer.appendChild(dateInput);
+
+    let priorityContainer = document.createElement('div');
+    priorityContainer.classList.add('card-priority-container');
+    cardContent.appendChild(priorityContainer);
+
+    let priorityLabel = document.createElement('label');
+    priorityLabel.classList.add('input-title');
+    priorityLabel.setAttribute('for', 'priority-input');
+    priorityLabel.innerHTML = 'Priority:';
+    priorityContainer.appendChild(priorityLabel);
+
+    let priorityInputContainer = document.createElement('select');
+    priorityInputContainer.classList.add('input-styling', 'card-input');
+    priorityInputContainer.setAttribute('type', 'text');
+    priorityInputContainer.setAttribute('id', 'priority-input');
+    priorityInputContainer.setAttribute('required', '');
+    priorityContainer.appendChild(priorityInputContainer);
+
+    let priorityOption1 = document.createElement('option');
+    priorityOption1.setAttribute('value', 'low');
+    priorityOption1.innerHTML = 'Low';
+    priorityInputContainer.appendChild(priorityOption1);
+
+    let priorityOption2 = document.createElement('option');
+    priorityOption2.setAttribute('value', 'medium');
+    priorityOption2.innerHTML = 'Medium';
+    priorityInputContainer.appendChild(priorityOption2);
+
+    let priorityOption3 = document.createElement('option');
+    priorityOption3.setAttribute('value', 'high');
+    priorityOption3.innerHTML = 'High';
+    priorityInputContainer.append(priorityOption3);
+
+    let projectContainer = document.createElement('div');
+    projectContainer.classList.add('card-project-container');
+    cardContent.appendChild(projectContainer);
+
+    let projectLabel = document.createElement('label');
+    projectLabel.classList.add('input-title');
+    projectLabel.setAttribute('for', 'project-input');
+    projectLabel.innerHTML = 'Project:';
+    projectContainer.appendChild(projectLabel);
+
+    let projectInputContainer = document.createElement('select');
+    projectInputContainer.classList.add('input-styling', 'card-input');
+    projectInputContainer.setAttribute('type', 'text');
+    projectInputContainer.setAttribute('id', 'project-input');
+    projectInputContainer.setAttribute('required', '');
+    projectContainer.appendChild(projectInputContainer);
+
+    let projectOption1 = document.createElement('option');
+    projectOption1.setAttribute('value', 'inbox');
+    projectOption1.innerHTML = 'Inbox';
+    projectInputContainer.appendChild(projectOption1);
+
+    let submissionBtnsContainer = document.createElement('div');
+    submissionBtnsContainer.classList.add('submission-btns-container');
+    cardContentContainer.appendChild(submissionBtnsContainer);
+
+    let resetBtn = document.createElement('button');
+    resetBtn.classList.add('reset-btn');
+    resetBtn.setAttribute('type', 'reset');
+    resetBtn.innerHTML = 'Reset Task';
+    submissionBtnsContainer.appendChild(resetBtn);
+
+    let submitBtn = document.createElement('button');
+    submitBtn.classList.add('submit-btn');
+    submitBtn.setAttribute('type', 'submit');
+    submitBtn.innerHTML = 'Add Task';
+    submissionBtnsContainer.appendChild(submitBtn);
+
+    return card;
+}
 
 function createSimpleNote() {
     let noteContainer = document.createElement('div');
