0629f100093240f930cf1332cc2d075c00b0b538
Edgar Carrillo <ecarrillo046@gmail.com>
2021-07-30 08:39:07 -0700

    Have pg title update when tab is changed

--- src/modules/tab-logic.js
+++ src/modules/tab-logic.js
@@ -6,22 +6,26 @@ const tabPagePopulation = (() => {
     const thisWeekTab = document.querySelector('.this-week-tab');
     const projectsTab = document.querySelector('.projects-tab');
     const tabArr = [inboxTab, todayTab, thisWeekTab, projectsTab];
+    let pgTitle = document.querySelector('.title');
 
     inboxTab.addEventListener('click', () => {
         tabArr.forEach(tab => {tab.style.fontWeight = 'normal'});
         inboxTab.style.fontWeight = 'bold';
+        pgTitle.innerHTML = 'Inbox';
         switchTabs();
     });
 
     todayTab.addEventListener('click', () => {
         tabArr.forEach(tab => {tab.style.fontWeight = 'normal'});
         todayTab.style.fontWeight = 'bold';
+        pgTitle.innerHTML = 'Today';
         switchTabs()
     });
 
     thisWeekTab.addEventListener('click', () => {
         tabArr.forEach(tab => {tab.style.fontWeight = 'normal'});
         thisWeekTab.style.fontWeight = 'bold';
+        pgTitle.innerHTML = 'This Week';
         switchTabs()
     });
 
